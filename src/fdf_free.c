7d3edac6bbe07d526971dcbbc4f5dada0939e7a8
snakajim <snakajim@student.42tokyo.jp>
2025-01-01 14:24:25 +0900

    ADD: fdf_free

--- src/fdf_free.c
+++ src/fdf_free.c
@@ -14,5 +14,13 @@ void free_split(char **split)
 
 void fdf_free(t_fdf *fdf)
 {
+	int i;
 
+	i = 0;
+	if (fdf->map.points)
+	{
+		while (fdf->map.points[i])
+			free(fdf->map.points[i++]);
+		free(fdf->map.points);
+	}
 }
